a5e070250e16777ce3964741f2e1b41f61392ab2
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-08-30 22:11:05 -0400

    Added new game button functionality, prompt for character name

--- public/main.js
+++ public/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
   $('.hidden').fadeIn(1000).removeClass('hidden');
 
+  let player = {
+    name: null,
+    health: 250,
+    energy: 100,
+    mana: 100,
+    inventory: {}
+  }
+
+  const setStage = () => {
+    player.name = prompt('Please enter your character name:')
+    $(characterName).text(player.name)
+    $(healthBar).animate({
+      width: '100%'
+    }, 500).text(player.health)
+    $(energyBar).animate({
+      width: '100%'
+    }, 500)
+    $(manaBar).animate({
+      width: '100%'
+    }, 500)
+  }
+
+  $(newGameButton).click(function(){
+    console.log('new battle, reset player bars')
+    setStage()
+  })
+
+  $(attackButton).click(function(){
+    console.log('attack, append new attack event to the even log, and consume some energy')
+  })
   
 })
