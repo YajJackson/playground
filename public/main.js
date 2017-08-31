f57105a6a2b76820414166b896a769daac73b6fd
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-08-30 22:53:10 -0400

    Added attack button functionality, spends energy and updates progress bar. Need to add way to regenerate energy.

--- public/main.js
+++ public/main.js
@@ -2,34 +2,65 @@ $(document).ready(function(){
   $('.hidden').fadeIn(1000).removeClass('hidden');
 
   let player = {
-    name: null,
-    health: 250,
-    energy: 100,
-    mana: 100,
+    name: '',
+    health: 0,
+    energy: 0,
+    mana: 0,
     inventory: {}
   }
 
   const setStage = () => {
     player.name = prompt('Please enter your character name:')
+    player.health = 100
+    player.energy = 100
+    player.mana = 100
+
     $(characterName).text(player.name)
     $(healthBar).animate({
-      width: '100%'
+      width: `${player.health}%`
     }, 500).text(player.health)
     $(energyBar).animate({
-      width: '100%'
+      width: `${player.energy}%`
     }, 500)
     $(manaBar).animate({
-      width: '100%'
+      width: `${player.mana}%`
     }, 500)
   }
 
   $(newGameButton).click(function(){
-    console.log('new battle, reset player bars')
+    console.log('new battle, reset player values and update bars')
     setStage()
   })
 
+  const tryAttack = (cost) => {
+    if(cost <= player.energy){
+      player.energy -= cost
+      $(energyBar).animate({
+        width: `${player.energy}%`
+      })
+      return true
+    } else {
+      alert('Not enough energy')
+      return false
+    }
+  }
+  // <li class="list-group-item list-group-item-danger">This is a danger list group item</li>
+  const attackEvent = (success, result) => {
+    if(success){
+      let li = document.createElement('li')
+      $(li).addClass("list-group-item list-group-item-danger").text(result)
+      $(eventLog).append(li)
+    }
+  }
+
   $(attackButton).click(function(){
-    console.log('attack, append new attack event to the even log, and consume some energy')
+    if(tryAttack(15)){
+      console.log('Attack successful')
+      attackResult = `${player.name} dealt 20 damage.`
+      attackEvent(true, attackResult)
+    } else {
+      console.log('Attack failed')
+    }
   })
   
 })
